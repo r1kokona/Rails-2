35b0a2da739be80845468aa60b0e162d2503be79
2402043 <>
2024-07-22 19:24:11 +0900

    setup

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
+require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  def setup
+    Capybara.server_host = "0.0.0.0"
+    Capybara.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}" if ENV["CHROMIUM_REMOTE_URL"].present?
+    super
+  end
+  driven_by :cuprite, options: { browser: :remote, url: ENV["CHROMIUM_REMOTE_URL"] }
 end
