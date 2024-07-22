4ea409d8d2c71b8f77cee2cd6ce7fcff86a313dd
2402043 <>
2024-07-22 19:24:37 +0900

    setup

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,16 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "Users"
-  # end
+  setup do
+    Capybara.javascript_driver = :cuprite
+    Capybara.register_driver(:cuprite) do |app|
+      Capybara::Cuprite::Driver.new(app, browser_options: { "no-sandbox": nil })
+    end
+    
+  end
+  test "visiting the index" do
+    visit users_url
+  
+    assert_selector "h1", text: "Users"
+  end
 end
