0fa3caef6c886827959be777b82f371d70a36ef7
2402043 <>
2024-07-22 17:16:31 +0900

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(article_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
   def user_params
