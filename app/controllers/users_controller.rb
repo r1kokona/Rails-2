e3093bfe4289129a237f225d3da7629d76b099d1
2402043 <>
2024-07-22 18:07:20 +0900

    destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destory
+    @user = User.find([:id])
+    @user.destory
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def user_params
     params.require(:user).permit(:name)
