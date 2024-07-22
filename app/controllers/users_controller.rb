30da1b52331dc7d7784eea6197dcb44a8cf71dc1
2402043 <>
2024-07-22 16:29:09 +0900

    new , create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(name: "aaa")
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
