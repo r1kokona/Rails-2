7e90aaefa582e44c8d3293517d659b772269ed2e
2402043 <>
2024-07-24 09:42:28 +0900

    test名は日本語で

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+  test "indexを取得する" do
+    get users_path
     assert_response :success
   end
 end
