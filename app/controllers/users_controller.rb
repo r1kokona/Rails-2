f299a77fd1668a1b22909f32bce96c468af04813
2402043 <>
2024-07-24 15:18:21 +0900

    [fix]User.new[user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(name: "aaa")
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
