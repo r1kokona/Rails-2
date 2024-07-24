e2d7c3feb73feadfe5d8608f11900ee0a6e76fc3
2402043 <>
2024-07-24 14:39:55 +0900

    model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(name:"Example User")
+  end
+  test "有効である" do
+    assert @user.valid?
+  end
+  test "存在する名前である" do
+    @user.name = "   "
+    assert_not_nil @user.valid?
+  end
 end
