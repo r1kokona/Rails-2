82ffa854e9e4dc844d7f8c9f5d47026c980b9215
2402043 <>
2024-07-24 15:36:58 +0900

    [add] systemtest

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -6,11 +6,33 @@ class UsersTest < ApplicationSystemTestCase
     Capybara.register_driver(:cuprite) do |app|
       Capybara::Cuprite::Driver.new(app, browser_options: { "no-sandbox": nil })
     end
-    
+    @user = users(:one)
   end
   test "visiting the index" do
     visit users_url
   
     assert_selector "h1", text: "Users"
   end
+  test "ユーザー作成" do
+    visit users_url
+    click_on "New User"
+
+    fill_in "Name", with: "New User"
+    click_on "Create User"
+    # リダイレクトする？
+  end
+  test "ユーザー情報アップデート" do
+    visit users_url
+    click_on "Edit", match: :first
+
+    fill_in "Name", with: "Updated User"
+    click_on "Update User"
+  end
+  test "ユーザー削除" do
+    visit users_url(@user)
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+  end
+
 end
