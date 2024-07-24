172a62092d8ec2ebdf2cd5b450bf3a2c9455a8cf
2402043 <>
2024-07-24 09:55:49 +0900

    indexを取得

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "indexを取得する" do
-    get users_path
+    get users_index_url
     assert_response :success
   end
 end
