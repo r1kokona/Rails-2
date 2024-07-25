c9961d0461cfbffe52629dc198ffa5c7e7981694
2402043 <>
2024-07-25 22:14:55 +0900

    system test !driverがない

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 RSpec.describe "viewing the h1", type: :system do
+    # before do
+    #     driven_by(:selenium_chrome)
+    # end
     it "h1" do
         visit root_path
         expect(page).to have_content "Users"
