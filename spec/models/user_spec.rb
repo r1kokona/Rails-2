6a1348c0cb0277dc943c0042bbe34e2200da7593
2402043 <>
2024-07-25 21:45:10 +0900

    model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe User, type: :model do
+    fixtures :users
+    let(:user) { users(:one)}
+
+    describe "validations" do
+        it "有効な属性か" do
+            expect(user).to be_valid
+        end
+        it "名前がない時無効" do
+            user.name = nil
+            expect(user).to_not be_valid
+        end
+        it "名前が空白の場合は無効" do
+            user.name = " "
+            expect(user).to_not be_valid
+        end
+        describe "database" do
+            it "名前のカラムがある" do
+                expect(User.column_names).to include("name")
+            end
+        end
+    end
+end
