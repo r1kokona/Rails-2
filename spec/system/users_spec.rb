68ee3b5cfb9954e5e7da19be94713550815378e0
2402043 <>
2024-07-26 12:35:12 +0900

    delete driver

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,10 +1,49 @@
 require "rails_helper"
-RSpec.describe "viewing the h1", type: :system do
-    # before do
-    #     driven_by(:selenium_chrome)
-    # end
+    let!(:user) { User.create(name: "Test User") }
+
     it "h1" do
         visit root_path
         expect(page).to have_content "Users"
     end
+    describe 'index' do
+        it 'ユーザーリストを表示する' do
+          visit users_path
+          expect(page).to have_content('Test User')
+        end
+      end
+    
+      describe 'show' do
+        it 'displays a specific user' do
+          visit user_path(user)
+          expect(page).to have_content('Test User')
+        end
+      end
+    
+      describe '新しいユーザー' do
+        it '新しいユーザーを産みだす' do
+          visit new_user_path
+          fill_in 'Name', with: 'New User'
+          click_button 'Create User'
+          expect(page).to have_content('New User')
+        end
+      end
+    
+      describe 'ユーザー情報を修正する' do
+        it 'ユーザー名を書き換える' do
+          visit edit_user_path(user)
+          fill_in 'Name', with: 'Updated User'
+          click_button 'Update User'
+          expect(page).to have_content('Updated User')
+        end
+      end
+    
+      describe 'ユーザー削除' do
+        it 'ユーザーを削除する' do
+          visit users_path
+          accept_confirm do
+            click_link 'Destroy', href: users_path(user)
+          end
+          expect(page).not_to have_content('Test User')
+        end
+      end
 end
