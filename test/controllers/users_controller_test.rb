49ff41b035eea591c2582db98a7f1c3ec1391fba
2402043 <>
2024-07-29 15:29:44 +0900

    Create, update, destroy

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,29 +4,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
-  test "indexを取得する" do
-    get users_url
-    assert_response :success
-    # assert_not_nil assigns(:users)
-  end
-  test "newを取得する" do
-    get new_user_url
-    assert_response :success
-  end
   test "ユーザー作成できる" do
     assert_difference('User.count') do
       post users_url, params: { user: {name: "taro"}}
     end
 
     assert_redirected_to user_url(User.last)
-  end
-  test "ユーザーをshowできる" do
-    get user_url(@user)
-    assert_response :success
-  end
-  test "editを取得する" do
-    get edit_user_url(@user)
-    assert_response :success
   end
   test "ユーザーをupdateできる" do
     patch user_url(@user), params: { user: { name: "iii"}}
