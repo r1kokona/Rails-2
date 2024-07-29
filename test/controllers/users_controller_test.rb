5da5caa3ddec1e438903bd460af7c7d339db2d28
2402043 <>
2024-07-29 16:03:07 +0900

    fix path

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,18 +6,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
   test "ユーザー作成できる" do
     assert_difference('User.count') do
-      post users_url, params: { user: {name: "taro"}}
+      post users_path, params: { user: {name: "taro"}}
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to user_path(User.last)
   end
   test "ユーザーをupdateできる" do
     patch user_url(@user), params: { user: { name: "iii"}}
-    assert_redirected_to user_url(@user)
+    assert_redirected_to user_path(@user)
   end
   test "ユーザーをdestroyできる" do
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_path(@user)
     end
 
     assert_redirected_to root_path
