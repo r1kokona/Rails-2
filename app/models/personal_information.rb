64d3d8fc27b5145aa33a27cb53e9fb42d53fc022
2402043 <>
2024-08-01 18:38:39 +0900

    validates

--- app/models/personal_information.rb
+++ app/models/personal_information.rb
@@ -3,14 +3,10 @@ class PersonalInformation < ApplicationRecord
     validates :name, presence: true
     validates :ruby, presence: true
     validates :sex, presence: true, inclusion: { in: %w(男 女), message: "%{value} is not a valid sex" }
-    validates :tel, presence: true, format: { with: /\A\d{10,11}\z/, message: "must be a valid phone number" }, length: { is: 10 }
-    validates :mobile, presence: true, format: { with: /\A\d{10,11}\z/, message: "must be a valid mobile number" }, length: { is: 10 }
+    validates :tel, allow_blank: true, format: { with: /\A0[-\d]{9,12}\z/, message: "must be a valid phone number" }
+    validates :mobile, allow_blank: true, format: { with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "must be a valid mobile number" }
     validates :mail, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-    validates :zip, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "must be a valid postal code" }, length: { is: 7 }
+    validates :zip, presence: true, format: { with: /\A\d{3}-?\d{4}\z/, message: "must be a valid postal code" }  
     validates :address1, presence: true
-    validates :address2, presence: true
-    validates :address3, allow_blank: true
-    validates :address4, allow_blank: true
-    validates :address5, allow_blank: true
     validates :birthday, presence: true
 end
