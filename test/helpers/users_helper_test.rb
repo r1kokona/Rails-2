be3ade755cefe05449757f076d9f6857146bc888
2402043 <>
2024-08-09 16:29:48 +0900

    @user.name

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
   setup do
-    @user = User.new(name: "Example User")
+    @user = users(:mitsui)
   end
 
   test "should format user name" do
-    assert_equal "User: Example User", format_user_name(@user)
+    assert_equal "User: #{@user.name}", format_user_name(@user)
   end
 
   test "should return user greeting" do
-    assert_equal "Hello, Example User!", user_greeting(@user)
+    assert_equal "Hello, #{@user.name}!", user_greeting(@user)
   end
 end
