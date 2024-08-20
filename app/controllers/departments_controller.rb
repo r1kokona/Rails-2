98a5261ab81eca8d43092a813edfcc176b39c285
2402043 <>
2024-08-20 16:23:55 +0900

    departments controller

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,40 @@
 class DepartmentsController < ApplicationController
+    def index
+        @departments = Department.all
+    end
+    def show
+        @department = Department.find(params[:id])
+    end
+    def new
+        @department = Department.new
+    end
+    def create
+        @department = Department.new(department_params)
+        if @department.save
+            redirect_to @department
+        else
+            render :new, staus: :unprocessable_entity
+        end
+    end
+    def edit
+        @department = Department.find(params[:id])
+    end
+    def update
+        @department = Department.find(params[:id])
+    
+        if department.update(department_params)
+            redirect_to @department
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    def destroy
+        @department = Department.find(params[:id])
+        @department.destroy
+        redirect_to root_path, status: :see_other
+    end
+    private
+    def department_params
+        params.require(:department).permit(department_id, department_name)
+    end
 end
