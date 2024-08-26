93ea78058ab87383fb18d97679511efd0055fc6b
2402043 <>
2024-08-26 15:00:02 +0900

    部署のシステムテスト

---
+++ test/system/department_test.rb
@@ -0,0 +1,36 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  setup do
+    Capybara.javascript_driver = :cuprite
+    Capybara.register_driver(:cuprite) do |app|
+      Capybara::Cuprite::Driver.new(app, browser_options: { "no-sandbox": nil })
+    end
+    @department = departments(:jinjibu)
+  end
+  test "Departmentが表示される" do
+    visit departments_path
+  
+    assert_selector "h1", text: "Departments"
+  end
+  test "部署作成" do
+    visit departments_path
+    click_on "New Department"
+
+    fill_in "Department name", with: "New Department"
+    click_on "Create Department"
+  end
+  test "部署情報アップデート" do
+    visit department_path(@department)
+    click_on "Edit", match: :first
+  
+    fill_in "Department name", with: "Updated Department"
+    click_on "Update Department"
+  end
+  test "部署情報削除" do
+    visit department_path(@department)
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+  end
+end
