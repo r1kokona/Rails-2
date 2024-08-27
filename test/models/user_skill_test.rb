573b3d6a90cca52146c87364157edbbb3446d50d
2402043 <>
2024-08-27 10:24:14 +0900

    7行目が怪しい

--- test/models/user_skill_test.rb
+++ test/models/user_skill_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class UserSkillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:mitsui)
+    @skill = skills(:typing)
+    @user_skill = UserSkill.new(user: @user, skill: @skill)
+  end
+
+  test "有効なUserSkillオブジェクトを持つべきである" do
+    assert @user_skill.valid?
+  end
+
+  test "ユーザーが関連付けられている必要がある" do
+    @user_skill.user = nil
+    assert_not @user_skill.valid?
+    assert_includes @user_skill.errors[:user], "can't be blank"
+  end
+
+  test "スキルが関連付けられている必要がある" do
+    @user_skill.skill = nil
+    assert_not @user_skill.valid?
+    assert_includes @user_skill.errors[:skill], "can't be blank"
+  end
+
+  test "ユーザーと正しく関連付けられている" do
+    assert_equal @user_skill.user, @user
+  end
+
+  test "スキルと正しく関連付けられている" do
+    assert_equal @user_skill.skill, @skill
+  end
 end
