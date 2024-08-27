364b817feac51eed6f6e28851227514bc8308d53
2402043 <>
2024-08-27 10:24:27 +0900

    resource :skills

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users
   resources :departments
+  resources :skills
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
