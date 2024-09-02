bcd33721b20f71950301cd739b206c2205f2871a
2402043 <>
2024-09-02 11:39:38 +0900

    user index/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def index
-  end
+    @users = User.all
+end
+def show
+    @user = User.find(params[:id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
+
+  resources :users
+
   namespace :admin do
     resources :users
     resources :departments
