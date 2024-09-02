6e239859a6f0c75ca29a121b6fb4c0fc7749c253
2402043 <>
2024-09-02 10:47:30 +0900

    namespace :admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users
-  resources :departments
-  resources :skills
+  root 'admin/users#index'
+  namespace :admin do
+    resources :users
+    resources :departments
+    resources :skills
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
