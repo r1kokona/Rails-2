3e1b72bc17696b1782413c7856ede3d6430ab8a6
2402043 <>
2024-09-02 10:31:51 +0900

    [update] admin/departments

---
+++ app/controllers/admin/departments_controller.rb
@@ -0,0 +1,40 @@
+class Admin::DepartmentsController < ApplicationController
+    def index
+        @departments = Department.all
+    end
+    def show
+        @department = Department.find(params[:id])
+    end
+    def new
+        @department = Department.new
+    end
+    def create
+        @department = Department.new(department_params)
+        if @department.save
+            redirect_to admin_department_path(@department)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    def edit
+        @department = Department.find(params[:id])
+    end
+    def update
+        @department = Department.find(params[:id])
+    
+        if @department.update(department_params)
+            redirect_to admin_department_path(@department)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    def destroy
+        @department = Department.find(params[:id])
+        @department.destroy
+        redirect_to root_path, status: :see_other
+    end
+    private
+    def department_params
+        params.require(:department).permit(:department_name)
+    end
+end
